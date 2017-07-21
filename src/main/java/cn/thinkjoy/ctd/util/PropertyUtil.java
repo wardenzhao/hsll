5fcf77d8b25133d083f9db47e414c7aef375c1e5
赵一凡 <674097325@qq.com>
2017-07-21 22:06:41 +0800

    整改

--- src/main/java/cn/thinkjoy/ctd/util/PropertyUtil.java
+++
@@ -1,9 +0,0 @@
-package cn.thinkjoy.ctd.util;
-
-/**
- * Created by wpliu on 16/3/2.
- */
-public class PropertyUtil {
-
-
-}
---
+++ src/main/java/cn/thinkjoy/hsll/controller/TestController.java
@@ -0,0 +1,34 @@
+package cn.thinkjoy.hsll.controller;
+
+/**
+ * Created by wpliu on 16/3/2.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 点击拨号控制类
+ */
+@Scope("prototype")
+@Controller("testController")
+@RequestMapping(value = "/hsll")
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String clickView(HttpServletRequest request){
+
+        return "asdsa";
+    }
+
+
+}
